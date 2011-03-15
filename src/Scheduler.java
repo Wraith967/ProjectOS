dae841394281df021bda0e96735c170d347b1985
BenBern <benbernhardt@mindspring.com>
2011-03-15 13:54:43 -0400

    Minor changes, SJF implemented

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -15,19 +15,22 @@ public class Scheduler {
 	 * i,j = indices
 	 * dest = used to find actual address for memory
 	 */
-	int jobBegin, i, j, dest;
+	int jobBegin, i, j, k, dest;
 	char[][] disk;
 	PCB[] p;
+	int[] rq;
 	
-	public Scheduler(MemoryManager mgr, char[][] disk, PCB[] p)
+	public Scheduler(MemoryManager mgr, char[][] disk, PCB[] p, int[] rq)
 	{
 		this.mgr = mgr;
 		jobBegin = -1;
 		i = -1;
 		j = -1;
+		k = -1;
 		dest = -1;
 		this.disk = disk;
 		this.p = p;
+		this.rq = rq;
 	}
 	
 	/**
@@ -36,53 +39,91 @@ public class Scheduler {
 	 * @param p
 	 * @param disk
 	 */
-	public void LoadMulti(int[] rq)
+	public void LoadMulti(int run)
 	{
 		dest = 0;
+		if (run == 0) k = 0;
+		else k = 15;
 		for (i=0; i<rq.length; i++)
 		{
-			jobBegin = p[i].beginIndex;
+			jobBegin = p[k].beginIndex;
 			if (jobBegin == -1)
 			{
 				System.out.println("Invalid PCB");
 			}
 			else
 			{
-				p[i].diskEnd = System.nanoTime();
-				rq[i] = dest;
-				p[i].base_Register = dest; // modify for m-scheduler later
-				for (j=0; j<p[i].totalSize; j++)
+				p[k].diskEnd = System.nanoTime();
+				rq[i] = p[k].jobID;
+				p[k].base_Register = dest; // modify for m-scheduler later
+				for (int j=0; j<p[k].totalSize; j++)
 				{
 					mgr.WriteInstruction(dest+j,disk[jobBegin+j]);
 				}
-				dest += 72;
-				p[i].readyStart = System.nanoTime();
+				dest += p[k].totalSize;
+				p[k++].readyStart = System.nanoTime();
 			}
 		}
+		mergeSort(rq);
+//		for (i=0; i<rq.length; i++)
+//			System.out.println(rq[i]);
 	}
 	
 	/**
-	 * 
-	 * @param rq
-	 * @param p
-	 * @param disk
-	 * @param index
-	 */
-	public void LoadSingle(int[] rq, PCB p, int index)
-	{
-		//Dispatcher.threadMessage("LoadSingle" + index);
-		p.diskEnd = System.nanoTime();
-		jobBegin = p.beginIndex;
-		if (jobBegin == -1)
-		{
-			System.out.println("Invalid PCB");
-		}
-		else
-		{
-			p.base_Register = rq[index];
-			for (i=0; i<p.totalSize; i++)
-				mgr.WriteInstruction(rq[index] + i, disk[jobBegin+i]);
-			p.readyStart = System.nanoTime();
-		}
-	}
+     * Mergesort algorithm.
+     * @param a an array of Comparable items.
+     */
+    private void mergeSort( int [] a ) {
+        int [] tmpArray = new int[ a.length ];
+        mergeSort( a, tmpArray, 0, a.length - 1 );
+    }
+    
+    /**
+     * Internal method that makes recursive calls.
+     * @param a an array of Comparable items.
+     * @param tmpArray an array to place the merged result.
+     * @param left the left-most index of the subarray.
+     * @param right the right-most index of the subarray.
+     */
+    private void mergeSort( int [ ] a, int [ ] tmpArray,
+            int left, int right ) {
+        if( left < right ) {
+            int center = ( left + right ) / 2;
+            mergeSort( a, tmpArray, left, center );
+            mergeSort( a, tmpArray, center + 1, right );
+            merge( a, tmpArray, left, center + 1, right );
+        }
+    }
+    
+    /**
+     * Internal method that merges two sorted halves of a subarray.
+     * @param a an array of Comparable items.
+     * @param tmpArray an array to place the merged result.
+     * @param leftPos the left-most index of the subarray.
+     * @param rightPos the index of the start of the second half.
+     * @param rightEnd the right-most index of the subarray.
+     */
+    private void merge( int [ ] a, int [ ] tmpArray,
+            int leftPos, int rightPos, int rightEnd ) {
+        int leftEnd = rightPos - 1;
+        int tmpPos = leftPos;
+        int numElements = rightEnd - leftPos + 1;
+        
+        // Main loop
+        while( leftPos <= leftEnd && rightPos <= rightEnd )
+            if( p[a[leftPos]-1].codeSize < p[a[rightPos]-1].codeSize  )
+                tmpArray[ tmpPos++ ] = a[ leftPos++ ];
+            else
+                tmpArray[ tmpPos++ ] = a[ rightPos++ ];
+        
+        while( leftPos <= leftEnd )    // Copy rest of first half
+            tmpArray[ tmpPos++ ] = a[ leftPos++ ];
+        
+        while( rightPos <= rightEnd )  // Copy rest of right half
+            tmpArray[ tmpPos++ ] = a[ rightPos++ ];
+        
+        // Copy tmpArray back
+        for( int i = 0; i < numElements; i++, rightEnd-- )
+            a[ rightEnd ] = tmpArray[ rightEnd ];
+    }
 }
