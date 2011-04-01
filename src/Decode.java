ff843695cb56f5c62387c5438a88db3399322322
Wraith967 <benbernhardt@mindspring.com>
2011-03-31 21:19:01 -0400

    Decode as switch

--- src/Decode.java
+++ src/Decode.java
@@ -26,37 +26,38 @@ public class Decode {
 	{
 		for (int i=0; i<8; i++)
 			converted[i] = HexToInt.convertHextoInt(a[i]);
-		if ((converted[0]==0)||(converted[0]==1))
+		switch (converted[0])
 		{
-			decInst[0]=0;
-			decInst[1]=converted[0]*16+converted[1];
-			decInst[2]=converted[2];
-			decInst[3]=converted[3];
-			decInst[4]=converted[4];
-		}
-		else if ((converted[0]==4)||(converted[0]==5))
-		{
-			decInst[0]=1;
-			decInst[1]=(converted[0]%4)*16+converted[1];
-			decInst[2]=converted[2];
-			decInst[3]=converted[3];
-			decInst[4]=converted[4]*4096+converted[5]*256+converted[6]*16+converted[7];
-		}
-		else if (converted[0]==9)
-		{
-			decInst[0]=2;
-			decInst[1]=(converted[0]%8)*16+converted[1];
-			decInst[2]=converted[2]*1048576+converted[3]*65536+converted[4]*4096+converted[5]*256+converted[6]*16+converted[7];
-			decInst[3]=0;
-			decInst[4]=0;
-		}
-		else
-		{
-			decInst[0]=3;
-			decInst[1]=(converted[0]%12)+converted[1];
-			decInst[2]=converted[2];
-			decInst[3]=converted[3];
-			decInst[4]=converted[4]*4096+converted[5]*256+converted[6]*16+converted[7];
+			case 0:
+			case 1:
+				decInst[0]=0;
+				decInst[1]=converted[0]*16+converted[1];
+				decInst[2]=converted[2];
+				decInst[3]=converted[3];
+				decInst[4]=converted[4];
+				break;
+			case 4:
+			case 5:
+				decInst[0]=1;
+				decInst[1]=(converted[0]%4)*16+converted[1];
+				decInst[2]=converted[2];
+				decInst[3]=converted[3];
+				decInst[4]=converted[4]*4096+converted[5]*256+converted[6]*16+converted[7];
+				break;
+			case 9:
+				decInst[0]=2;
+				decInst[1]=(converted[0]%8)*16+converted[1];
+				decInst[2]=converted[2]*1048576+converted[3]*65536+converted[4]*4096+converted[5]*256+converted[6]*16+converted[7];
+				decInst[3]=0;
+				decInst[4]=0;
+				break;
+			default:
+				decInst[0]=3;
+				decInst[1]=(converted[0]%12)+converted[1];
+				decInst[2]=converted[2];
+				decInst[3]=converted[3];
+				decInst[4]=converted[4]*4096+converted[5]*256+converted[6]*16+converted[7];
+				break;
 		}
 		return decInst;
 	}			
